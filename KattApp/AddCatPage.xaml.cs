f512869f8707c1b410f5fb6e0f93d132097ffd1a
Rebrick06 <gullstrand.rebecka@gmail.com>
2025-03-04 14:02:37 +0100

    addcatpage

--- KattApp/AddCatPage.xaml.cs
+++ KattApp/AddCatPage.xaml.cs
@@ -6,5 +6,10 @@ public partial class AddCatPage : ContentPage
 	{
 		InitializeComponent();
 	}
-   
+    private async void AddButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MyCatPage());
+        //skapar en ny cat page inte vad jag vill lös 
+    }
+
 }
--- KattApp/MyCatPage.xaml.cs
+++ KattApp/MyCatPage.xaml.cs
@@ -10,4 +10,22 @@ public partial class MyCatPage : ContentPage
     {
         await Navigation.PushAsync(new AddCatPage());
     }
+
+    public class Cat()
+    {
+        public string _name { get; set; }
+        public string _race { get; set; }
+        public int _bday { get; set; }
+        public int _weight { get; set; }
+        public string _coment { get; set; }
+
+        /*public Cat(string name, string race, int bday, int weight, string coment)
+        {
+            _name = name;
+            _race = race;
+            _bday = bday;
+            _weight = weight;
+            _coment = coment;
+        }*/
+    }
 }
