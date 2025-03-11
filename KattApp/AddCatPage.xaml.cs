d434beaab498eedbf5a87926255912cba3f69c2d
Rebrick06 <gullstrand.rebecka@gmail.com>
2025-03-11 13:11:18 +0100

    Update AddCatPage.xaml.cs

--- KattApp/AddCatPage.xaml.cs
+++ KattApp/AddCatPage.xaml.cs
@@ -8,7 +8,7 @@ public partial class AddCatPage : ContentPage
 	}
     private async void AddButton_Clicked(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new MyCatPage());
-        //skapar en ny cat page inte vad jag vill LÖS 
+        await Navigation.PopAsync();
+        
     }
 }
