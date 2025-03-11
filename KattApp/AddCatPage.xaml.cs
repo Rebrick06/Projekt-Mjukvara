704854e3f6fed970d3b24f9ec91b0b726104dff8
Rebrick06 <gullstrand.rebecka@gmail.com>
2025-03-11 12:43:04 +0100

    knapp löst

--- KattApp/AddCatPage.xaml.cs
+++ KattApp/AddCatPage.xaml.cs
@@ -8,8 +8,9 @@ public partial class AddCatPage : ContentPage
 	}
     private async void AddButton_Clicked(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new MyCatPage());
-        //skapar en ny cat page inte vad jag vill lös 
+        //går tilbacka till my cats
+        await Navigation.PopAsync();
+        
     }
 
 }
